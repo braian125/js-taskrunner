e2e36e9718039556c74c5e326efac5efd0b8cea9
Brian Vanegas <brianvanegas@viajesaeromar.com>
2017-12-11 16:47:33 -0500

    task runner

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+
+gulp.task('sass', function() {
+    return sass('resources/assets/sass/**/*.sass', {style: 'compressed'})
+      .pipe(concat('main.css'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('build-js', function() {
+    return gulp.src('resources/assets/js/*.js')    
+      .pipe(concat('main.js'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())      
+      .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('resources/assets/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('public/images'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('resources/assets/js/**/*.js', ['build-js']);
+  gulp.watch('resources/assets/sass/**/*.sass', ['sass']);
+  gulp.watch('resources/assets/images/**/*', ['images']);
+});
+
+gulp.task('default', ['build-js','sass','images', 'watch']);
